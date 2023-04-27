09f1d11d2652f3a18e8f51348df1ef32070fdeac
Bata Zoltán <batazo.dad@gmail.com>
2023-04-27 20:45:11 +0200

    Create config_sample.php

---
+++ APIs/SimpleDatabaseCheckAPI/config_sample.php
@@ -0,0 +1,16 @@
+<?php
+function TABLES() {
+	return [
+		'YOUR TABLE NAME' => 'YOUR TABLE NAME',
+    // ... You can add here more tables
+	];
+}
+
+function DBCONFIG() {
+	return [
+		'host' => 'YOUR host or localhost',
+		'user' => 'dbuser',
+		'pass' => 'dbuserpass',
+		'dbname' => 'databasename',
+	];
+}
