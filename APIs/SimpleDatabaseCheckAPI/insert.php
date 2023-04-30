3cfe2da717f632d8879dd9f1a269ba864c39cb2d
Bata Zoltán <batazo.dad@gmail.com>
2023-04-30 23:55:46 +0200

    Add files via upload
    	function insertAllToTable($tablename, $users) {
    		$dbh = $this->CONN();
    		if ($dbh) {
    			$sql = "INSERT INTO " . $tablename . " (Name, Email, age) VALUES (?, ?, ?)";
    			$stmt = $dbh->prepare($sql);
    
    			foreach ($users as $user) {
    				$stmt->execute([$user['Name'], $user['Email'], $user['Age']]);
    			}
    		}
    	}

---
+++ APIs/SimpleDatabaseCheckAPI/insert.php
@@ -0,0 +1,64 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//Config
+require_once 'config.php';
+
+//Model
+class DB {
+	private $dbconfig;
+
+	public function __construct($dbconfig) {
+		$this->dbconfig = $dbconfig;
+	}
+
+	public function CONN() {
+		try {
+			return new PDO('mysql:host=' . $this->dbconfig['host'] . ";dbname=" . $this->dbconfig['dbname'], $this->dbconfig['user'], $this->dbconfig['pass']);
+		} catch (PDOException $e) {
+			return false;
+		}
+	}
+
+	function queryAllFromTable($tablename) {
+		$sth = $this->CONN();
+		if ($sth) {
+			try {
+				$sql = "SELECT NODEHOST FROM " . $tablename;
+				$sth = $sth->query($sql);
+				return $sth->fetchAll(PDO::FETCH_ASSOC);
+			} catch (PDOException $e) {
+				return ['message' => $e];
+			}
+
+		}
+		return ['message' => "Database connection error"];
+	}
+
+	function insertAllToTable($tablename, $users) {
+		$dbh = $this->CONN();
+		if ($dbh) {
+			$sql = "INSERT INTO " . $tablename . " (Name, Email, age) VALUES (?, ?, ?)";
+			$stmt = $dbh->prepare($sql);
+
+			foreach ($users as $user) {
+				$stmt->execute([$user['Name'], $user['Email'], $user['Age']]);
+			}
+		}
+	}
+}
+
+$users = array(
+	array('Name' => 'John', 'Email' => 'john@example1.com', 'Age' => 25),
+	array('Name' => 'Jane', 'Email' => 'jane@example2.com', 'Age' => 30),
+	array('Name' => 'Bob', 'Email' => 'bob@example3.com', 'Age' => 40),
+	array('Name' => 'Alice', 'Email' => 'alice@example4.com', 'Age' => 35),
+	array('Name' => 'Tom', 'Email' => 'tom@example5.com', 'Age' => 27),
+);
+
+//CONTROLLER
+$db = new DB(DBCONFIG());
+$db->insertAllToTable("TestUser", $users);
